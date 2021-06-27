253d30561fe18e97251b2d1ac8ef3e1e3a23c9ac
Lane Kolbly <lane@rscheme.org>
2021-06-27 15:55:52 -0500

    Add StreamingAdc

--- imxrt-hal/src/adc.rs
+++ imxrt-hal/src/adc.rs
@@ -150,6 +150,10 @@ impl<ADCx> ADC<ADCx> {
         inst
     }
 
+    pub fn get_regs(&mut self) -> &ral::adc::Instance {
+        &self.reg
+    }
+
     /// Sets the resolution that analog reads return, in bits
     pub fn set_resolution(&mut self, bits: ResolutionBits) {
         ral::modify_reg!(ral::adc, self.reg, CFG, MODE: match bits {
@@ -280,6 +284,147 @@ where
 
     fn disable_source(&mut self) {
         ral::modify_reg!(ral::adc, self.adc.reg, GC, ADCO: 0, DMAEN: 0);
+    }
+}
+
+/// StreamingAdc wraps AdcSource and the DMA machinery to provide a nice
+/// user interface for reading regular samples from the ADC.
+///
+/// This implementation uses two linear buffers and switches between them,
+/// yielding 256 samples at a time.
+///
+/// # Example
+///
+/// ```no_run
+/// /*static ADC_BUFFER: hal::dma::Buffer<[u16; 256]> = hal::dma::Buffer::new([0; 256]);
+/// static ADC_BUFFER2: hal::dma::Buffer<[u16; 256]> = hal::dma::Buffer::new([0; 256]);
+/// static mut streaming_adc: Option<hal::adc::StreamingAdc<hal::iomuxc::adc::ADC1, t41::P24>> = None;
+///
+/// #[interrupt]
+/// fn DMA8_DMA24() {
+///     unsafe {
+///         if let Some(adc) = &mut streaming_adc {
+///             adc.handle_interrupt();
+///         }
+///     }
+/// }
+///
+/// ...
+///
+/// let (adc1_builder, _) = adc.clock(&mut ccm.handle);
+/// let mut adc1 = adc1_builder.build(
+///     adc::ClockSelect::default(),
+///     adc::ClockDivision::default()
+/// );
+/// let mut a1 = adc::AnalogInput::new(pins.p24);
+///
+/// let mut channel = dma_channels[8].take().unwrap();
+/// channel.set_interrupt_on_completion(true);
+/// unsafe {
+///     streaming_adc = Some(hal::adc::StreamingAdc::new(
+///         adc1,
+///         a1,
+///         channel,
+///         &ADC_BUFFER,
+///         &ADC_BUFFER2
+///     ));
+///     cortex_m::peripheral::NVIC::unmask(interrupt::DMA8_DMA24);
+/// }
+///
+/// ...
+///
+/// // Inside the main loop
+/// let mut sadc = unsafe { streaming_adc.as_mut().unwrap() };
+/// sadc.poll(|buf| {
+///     adc_min = *buf.iter().min().unwrap();
+///     adc_max = *buf.iter().max().unwrap();
+/// });*/
+/// ```
+pub struct StreamingAdc<ADCx, P> {
+    buffer0: &'static crate::dma::Buffer<[u16; 256]>,
+    buffer1: &'static crate::dma::Buffer<[u16; 256]>,
+    peripheral: crate::dma::Peripheral<
+        AdcSource<ADCx, P>,
+        u16,
+        crate::dma::Linear<u16>,
+        crate::dma::Linear<u16>,
+    >,
+    unhandled_buffer_count: core::sync::atomic::AtomicUsize,
+    off_buffer: crate::dma::Linear<u16>,
+    _pin: PhantomData<P>,
+}
+
+impl<ADCx, P> StreamingAdc<ADCx, P>
+where
+    ADCx: adc::ADC + AdcDmaSource,
+    P: Pin<ADCx>,
+{
+    /// Create a new StreamingAdc object. The returned object will not begin
+    /// generating samples until start() is called.
+    pub fn new(
+        adc: ADC<ADCx>,
+        pin: AnalogInput<ADCx, P>,
+        channel: crate::dma::Channel,
+        buffer0: &'static crate::dma::Buffer<[u16; 256]>,
+        buffer1: &'static crate::dma::Buffer<[u16; 256]>,
+    ) -> Self {
+        let mut off_buffer = crate::dma::Linear::new(&buffer0).unwrap();
+        off_buffer.set_transfer_len(256);
+        let mut peripheral = crate::dma::receive_u16(AdcSource::new(adc, pin), channel);
+
+        Self {
+            buffer0,
+            buffer1,
+            peripheral,
+            unhandled_buffer_count: core::sync::atomic::AtomicUsize::new(0),
+            off_buffer,
+            _pin: PhantomData,
+        }
+    }
+
+    /// Begin transferring samples from the ADC. Once samples are flowing, the
+    /// user must call poll() frequently enough to keep up with the generated
+    /// samples, or else the interrupt handler will panic.
+    pub fn start(&mut self) {
+        let mut buffer = crate::dma::Linear::new(&self.buffer1).unwrap();
+        buffer.set_transfer_len(256);
+        self.peripheral.start_receive(buffer).unwrap();
+    }
+
+    /// Process any received samples from the ADC. If no samples have been
+    /// received since the last poll() call, cb() will not be called.
+    pub fn poll<F: FnOnce(&[u16])>(&mut self, cb: F) {
+        if self
+            .unhandled_buffer_count
+            .load(core::sync::atomic::Ordering::SeqCst)
+            > 0
+        {
+            cb(self.off_buffer.as_elements());
+            self.unhandled_buffer_count
+                .store(0, core::sync::atomic::Ordering::SeqCst);
+        }
+    }
+
+    /// Called from an interrupt handler. The user must register the appropriate
+    /// DMA handler for their channel and call this method from it.
+    ///
+    /// If poll() is not called frequently enough to keep up with the data, this
+    /// handler will panic.
+    pub fn handle_interrupt(&mut self) {
+        if self.peripheral.is_receive_interrupt() {
+            self.peripheral.receive_clear_interrupt();
+        }
+
+        let mut rx_buffer = self.peripheral.receive_complete().unwrap();
+        core::mem::swap(&mut self.off_buffer, &mut rx_buffer);
+        self.peripheral.start_receive(rx_buffer).unwrap();
+        if self
+            .unhandled_buffer_count
+            .swap(1, core::sync::atomic::Ordering::SeqCst)
+            > 0
+        {
+            panic!("User is not processing ADC data fast enough!");
+        }
     }
 }
 
